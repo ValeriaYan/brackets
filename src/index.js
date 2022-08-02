80cc6e1b6d648f96de49b669d4878293f409fc0b
Valeria <valeria.yanycheva@gmail.com>
2022-08-02 16:41:00 +0600

    feat: do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsPairs = {}
+  for(let brackets of bracketsConfig){
+    bracketsPairs[brackets[0]] = brackets[1];
+  }
+
+  for(let br of str){
+    if(stack.length === 0){
+      if(`${br}` in bracketsPairs){
+        stack.push(br);
+      }else {
+        return false;
+      }
+    }else if(br == bracketsPairs[stack[stack.length - 1]]){
+      stack.pop();
+    }else if(`${br}` in bracketsPairs){
+      stack.push(br);
+    }
+  }
+  if(stack.length !== 0 ) return false;
+  return true;
 }
